e67089678f10d71e9e415d8f6bb987f66f40c9d2
Diego Díaz <diego_diaz9990@hotmail.com>
2023-05-23 19:59:06 -0300

    Componente "Home" Creado

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,46 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import homeStyles from "../../styles/home.module.css"
+import "../../styles/globals.css"
+import { josefinSans } from "./fonts.js"
+import BotonRegistro from "../../components/BotonRegistro/BotonRegistro"
 
 export default function Home() {
+
+  const botonRegistro = {
+    texto: 'Registrarse Gratis',
+    color: 'white',
+    backgroundColor: '#FF8E0A',
+    border: 'none',
+  }
+
+  const botonGoogle = {
+    texto: 'Continuar con Google',
+    color: '#26262E',
+    backgroundColor: 'rgba(255, 255, 255, 0.61)',
+    border: '2px solid black',
+  }
+
+  const botonApple = {
+    texto: 'Continuar con Apple',
+    color: '#26262E',
+    backgroundColor: 'rgba(255, 255, 255, 0.61)',
+    border: '2px solid black',
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className={homeStyles.main}>
+      <div id={homeStyles.contenedorNegro}>
+        <h1 id={homeStyles.logoAUDN} className={josefinSans.className}>audn</h1>
+      </div>
+      <h2 id={homeStyles.musicaMedida}>Música a medida</h2>
+      <div id={homeStyles.contenedorFinalHome}>
+        <div id={homeStyles.contenedorDeBotones}>
+          <BotonRegistro estilos={botonRegistro} />
+          <BotonRegistro estilos={botonGoogle} />
+          <BotonRegistro estilos={botonApple} />
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <p id={homeStyles.iniciarSesion}>Iniciar sesión</p>
       </div>
     </main>
   )
 }
+
