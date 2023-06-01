e38b06e9069dc0982407b7bf63a8a60e5063b014
Diego Díaz <diego_diaz9990@hotmail.com>
2023-06-01 13:49:49 -0300

    Modulo Inicio creado + estilos

--- src/app/inicio/page.js
+++ src/app/inicio/page.js
@@ -1,9 +1,87 @@
 import React from 'react'
+import inicio from '../../../styles/inicio.module.css'
+import Image from 'next/image'
 
 export default function Inicio() {
     return (
-        <>
-            <h1>Soy la página de inicio!</h1>
-        </>
+        <main id={inicio.main}>
+            <div id={inicio.primeraLinea}>
+                <h1>Música Ya!</h1>
+                <span>
+                    <Image
+                        src='/reciente.png'
+                        width={32}
+                        height={32}
+                        alt='recientes'
+                    />
+                    <Image
+                        src='/campana.png'
+                        width={32}
+                        height={32}
+                        alt='campana'
+                    />
+                </span>
+            </div>
+            <div className={inicio.contenedoresDeSecciones}>
+                <div className={inicio.contenedorNegro}>
+                    <Image
+                        src='/angel.png'
+                        width={60}
+                        height={70}
+                        alt='angel'
+                        id={inicio.angelImage}
+                    />
+                    <Image
+                        src='/auriculares.png'
+                        width={40}
+                        height={40}
+                        alt='auriculares'
+                        id={inicio.auricularesImage}
+                    />
+                </div>
+                <div className={inicio.contenedorTextos}>
+                    <h2>Cupido Musical</h2>
+                    <p className={inicio.textos}>Tus artistas favoritos nunca van a dejarte con el corazón roto.</p>
+                </div>
+            </div>
+            <div className={inicio.contenedoresDeSecciones}>
+                <div className={inicio.contenedorNegro}>
+                    <div id={inicio.contenedorPregunta}>
+                        <Image
+                            src='/signo-pregunta2.png'
+                            width={60}
+                            height={60}
+                            alt='signo-pregunta'
+                            id={inicio.signoPregunta2}
+                        />
+                        <Image
+                            src='/signo-pregunta.png'
+                            width={60}
+                            height={60}
+                            alt='signo-pregunta'
+                            id={inicio.signoPregunta}
+                        />
+                        <Image
+                            src='/signo-pregunta3.png'
+                            width={60}
+                            height={60}
+                            alt='signo-pregunta'
+                            id={inicio.signoPregunta3}
+                        />
+                    </div>
+                    <Image
+                        src='/mapa.png'
+                        width={40}
+                        height={40}
+                        alt='auriculares'
+                        id={inicio.mapaImagen}
+                    />
+                </div>
+                <div className={inicio.contenedorTextos}>
+                    <h2>Música Contextual</h2>
+                    <p className={inicio.textos}>Creamos la playlist perfecta para cualquier situación.</p>
+                </div>
+            </div>
+        </main>
     )
 }
