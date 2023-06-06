8fb93997727acb62793ea307cee418bf6244071b
Diego Díaz <diego_diaz9990@hotmail.com>
2023-06-06 04:00:20 -0300

    Componente Inicio responsive

--- src/app/inicio/page.js
+++ src/app/inicio/page.js
@@ -7,7 +7,7 @@ export default function Inicio() {
     return (
         <main id={inicio.main}>
             <div id={inicio.primeraLinea}>
-                <h1>Música Ya!</h1>
+                <h1 id={inicio.musicaYa}>Música Ya!</h1>
                 <span>
                     <Image
                         src='/reciente.png'
@@ -41,35 +41,20 @@ export default function Inicio() {
                     />
                 </div>
                 <div className={inicio.contenedorTextos}>
-                    <h2>Cupido Musical</h2>
+                    <h2 className={inicio.textosH2}>Cupido Musical</h2>
                     <p className={inicio.textos}>Tus artistas favoritos nunca van a dejarte con el corazón roto.</p>
                 </div>
             </div>
             <div className={inicio.contenedoresDeSecciones}>
                 <div className={inicio.contenedorNegro}>
-                    <div id={inicio.contenedorPregunta}>
-                        <Image
-                            src='/signo-pregunta2.png'
-                            width={60}
-                            height={60}
-                            alt='signo-pregunta'
-                            id={inicio.signoPregunta2}
-                        />
-                        <Image
-                            src='/signo-pregunta.png'
-                            width={60}
-                            height={60}
-                            alt='signo-pregunta'
-                            id={inicio.signoPregunta}
-                        />
-                        <Image
-                            src='/signo-pregunta3.png'
-                            width={60}
-                            height={60}
-                            alt='signo-pregunta'
-                            id={inicio.signoPregunta3}
-                        />
-                    </div>
+                    <Image
+                        src='/signo-pregunta.png'
+                        width={60}
+                        height={70}
+                        alt='signo-pregunta'
+                        id={inicio.signoPregunta}
+                    />
+
                     <Image
                         src='/mapa.png'
                         width={40}
@@ -79,7 +64,7 @@ export default function Inicio() {
                     />
                 </div>
                 <div className={inicio.contenedorTextos}>
-                    <h2>Música Contextual</h2>
+                    <h2 className={inicio.textosH2}>Música Contextual</h2>
                     <p className={inicio.textos}>Creamos la playlist perfecta para cualquier situación.</p>
                 </div>
             </div>
